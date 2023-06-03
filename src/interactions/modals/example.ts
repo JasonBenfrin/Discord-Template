bd1f802018c13f2098e5eed9b59162b71e44855a
Jason Benfrin <86666641+JasonBenfrin@users.noreply.github.com>
2023-06-03 18:42:10 +0300

    Fixed modal example

--- src/interactions/modals/example.ts
+++ src/interactions/modals/example.ts
@@ -1,8 +1,10 @@
 import BotModal from "../../structures/interactions/Modal.js";
 
-export default new BotModal({
-  name: 'example',
-  execute(interaction) {
-      
-  },
-})
+export default [
+  new BotModal({
+    name: 'example',
+    execute(interaction) {
+        
+    },
+  })
+]
